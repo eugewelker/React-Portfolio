b3f5293afce5eff763e2ba2c9aa6fec9b0518283
eugewelker <eugewelker88@gmail.com>
2019-10-14 13:33:59 +0100

    set state-show+-

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {displayBio:false};
+      this.toggleDisplayBio=this.toggleDisplayBio.bind(this);
+    }
+
+/*    readMore(){
+      this.setState({displayBio:true});
+    }
+
+    showLess()
+  {
+    this.setState({displayBio:false});
+  }*/
+
+  toggleDisplayBio(){
+    this.setState({displayBio:!this.state.displayBio})
+  }
+
+    render(){
+
+      return (
+      <div className="App">
+        <h1>My name is Eugenio. I am a 31 yo Engineer</h1>
+        <p>I am always looking forward to working on meaningful projects</p>
+        {this.state.displayBio?(
+          <div>
+            <p>I live in London, and coding every day</p>
+            <p> My favorite language is Javascript, and I think ReactJs is awesome</p>
+            <p>Besides coding, I also love RPG & card games, and running</p>
+            <button onClick={this.toggleDisplayBio}>Show Less</button>
+          </div>
+        ):(
+          <div>
+          <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )
+      }
+      </div>
+    )
+    }
 }
 
 export default App;
