b24fb093fa7d4a857e9a655872347d5bc269e4ca
eugewelker <eugewelker88@gmail.com>
2019-11-04 14:21:47 +0000

    add music master & evens-or-odds project

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,6 @@ import React from 'react';
 import{Link} from 'react-router-dom';
 
 const Header= ({children})=> {
-
-
     const style={
       display:'inline-block',
       margin:10,
@@ -15,6 +13,8 @@ const Header= ({children})=> {
       <div>
         <h3 style={style}><Link to ='/'>Home</Link></h3>
         <h3 style={style}><Link to ='/jokes'>Jokes</Link></h3>
+        <h3 style={style}><Link to ='/music-master'>Music Master</Link></h3>
+        <h3 style={style}><Link to ='/evens-or-odds'>Evens or Odds</Link></h3>
         </div>
         {children}
     </div>
--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
 import Jokes from './Jokes';
+import MusicMaster from './projects/music-master';
+import EvensOrOdds from './projects/evens-or-odds';
 import Header from './Header';
 import './index.css';
+
 
 ReactDOM.render(
   <Router history={createBrowserHistory()}>
     <Switch>
       <Route exact path='/' render= {() => <Header><App/></Header>}/>
       <Route path='/jokes' render= {() => <Header><Jokes/></Header>}/>
+      <Route path='/music-master' render= {() => <Header><MusicMaster/></Header>}/>
+      <Route path='/evens-or-odds' render= {() => <Header><EvensOrOdds/></Header>}/>
     </Switch>
   </Router>, document.getElementById('root')
 );
